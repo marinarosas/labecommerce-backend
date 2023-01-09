29f4e998a7bb7621504d8ddd54e42c60acc9feb2
Mjfreedom <mbuzina@gmail.com>
2023-01-09 12:54:44 -0300

    exercicio finalizado antes do feedback

---
+++ apis-e-express-exercicios-main/README.md
@@ -0,0 +1,24 @@
+# Exercício da tarde - APIs e Express
+
+ATENÇÃO! Não forke nem clone esse repositório. Ele serve apenas como enunciado!<br><br>
+
+<strong>Continue trabalhando no seu repositório labecommerce-backend criado anteriormente. Evite trabalhar na main e crie branches para organizar cada nova implementação ou refatoração. A de hoje por exemplo, pode se chamar apis-e-express.</strong>
+
+### Comando para criar a branch e já entrar nela
+```
+git checkout -b apis-e-express
+```
+<br>
+
+<strong>Obs:</strong> Caso não tenha criado seu repositório e/ou não tenha feito as atividades anteriores, <strong>siga as instruções dos repos abaixo:</strong> <br>
+
+- [Node e package.json](https://github.com/labenuexercicios/node-package-json-exercicios)
+- [Typescript I](https://github.com/labenuexercicios/typescript-i-exercicios)
+- [Typescript II](https://github.com/labenuexercicios/typescript-ii-exercicios)
+
+
+
+## Relembrando
+
+O objetivo dos exercícios da tarde é desenvolver o próprio projeto Labecommerce - Backend conforme o avanço das aulas.
+A cada atividade da tarde iremos implementar ou refatorar partes do projeto, além de praticar lógicas por fora.
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,86 @@
+import express, { application, Request, Response } from 'express'
+import cors from 'cors'
 import { products, users, purchase } from "./database"
+import { TProduct, TUsers, TPurchase } from './types'
 
 console.log('Hello world!')
 
-console.table(users)
-console.table(products)
-console.table(purchase)
+// console.table(users)
+// console.table(products)
+// console.table(purchase)
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003")
+})
+
+app.get('/ping', (req: Request, res: Response) => {
+    res.send('Pong!')
+})
+
+// ## Get All Users
+app.get("/users", (req: Request, res: Response)=>{
+    res.status(200).send(users)
+})
+
+// ## Get All Products
+app.get("/products", (req: Request, res: Response)=>{
+    res.status(200).send(products)
+})
+
+
+// ## Search Product by name
+app.get("/products/search", (req: Request, res: Response)=>{
+    const q = req.query.q as string
+    const results = products.filter((product)=>{
+        return product.name.toLowerCase().includes(q.toLowerCase())
+    })
+    res.status(200).send(results)
+})
+
+// ## Create User
+app.post("/users", (req: Request, res: Response)=>{
+    const {id, email, password} = req.body as TUsers
+
+    const newUser = {
+        id,
+        email,
+        password
+    }
+    users.push(newUser)
+    res.status(201).send("Cadastro realizado com sucesso")
+})
+
+// ## Create Product
+app.post("/products", (req: Request, res: Response) =>{
+    const {id, name, price, category} = req.body as TProduct
+    
+    const newProduct = {
+        id,
+        name,
+        price,
+        category
+    }
+
+    products.push(newProduct)
+    res.status(201).send("Produto cadastrado com sucesso")
+})
+
+// ## Create Purchase
+app.post("/purchases", (req: Request, res: Response)=>{
+    const {userId, productId, quantity, totalPrice} = req.body as TPurchase
+   
+    const newPurchase = {
+        userId,
+        productId,
+        quantity,
+        totalPrice
+    }
+    purchase.push(newPurchase)
+    res.status(201).send("Compra realizado com sucesso")
+})
+
